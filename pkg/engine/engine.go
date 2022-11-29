76f2ce2c5322606393a426572dd6564cec15807b
Krzysztof Kruk <krzysztof.kruk@grapeup.com>
2022-11-29 10:15:32 +0000

    BattleShipGameEngine constructor

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,8 +1,27 @@
 package engine
 
+import (
+	"battleship/pkg/board"
+	"battleship/pkg/game"
+)
+
+var instance *BattleShipGameEngine
+
 type BattleShipGameEngine struct {
-	games   interface{}
-	winners interface{}
+	b       *board.Board
+	games   []*game.Game
+	winners []string
+}
+
+func New(b *board.Board) *BattleShipGameEngine {
+	if instance == nil {
+		instance = &BattleShipGameEngine{
+			b:       b,
+			games:   []*game.Game{},
+			winners: []string{},
+		}
+	}
+	return instance
 }
 
 func (b *BattleShipGameEngine) Shoot(player, position string) (bool, error) {
--- pkg/engine/engine_test.go
+++ pkg/engine/engine_test.go
@@ -1,3 +1,49 @@
-package engine
+package engine_test
 
-// shoot searches through games to find one for player and makes a shot on that game
+import (
+	"battleship/pkg/board"
+	"battleship/pkg/engine"
+	"battleship/pkg/position"
+	"battleship/pkg/ship"
+	"testing"
+)
+
+func TestBattleShipGameEngine_New(t *testing.T) {
+	// should not create second instnce if there is one already
+	b := buildBoard()
+	e1 := engine.New(b)
+	e2 := engine.New(b)
+	if e1 != e2 {
+		t.Error("BattleShipGameEngine should be singleton but is not")
+	}
+}
+
+func TestBattleShipGameEngine_Shoot(t *testing.T) {
+}
+
+func TestBattleShipGameEngine_TopTen(t *testing.T) {
+}
+
+func buildBoard() *board.Board {
+	sps := buildShips()
+	return board.New(sps)
+}
+
+func buildShips() []*ship.Ship {
+	return []*ship.Ship{
+		buildShip("A1", "A2", "A3"),
+		buildShip("B1", "B2"),
+	}
+}
+
+func buildShip(coordinates ...string) *ship.Ship {
+	return ship.New(buildPositions(coordinates...))
+}
+
+func buildPositions(coordinates ...string) []*position.Position {
+	var p []*position.Position
+	for _, c := range coordinates {
+		p = append(p, position.New(c))
+	}
+	return p
+}
